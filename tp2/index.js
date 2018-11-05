402caf205de0bd9073f918c39d9089346d48e72c
lampthiam <44144146+lampthiam@users.noreply.github.com>
2018-11-05 18:00:04 +0100

    Add files via upload

--- tp2/index.js
+++ tp2/index.js
@@ -1,36 +1,26 @@
+function validation(){
+    var isError = false;
+    var divs = document.querySelectorAll('input.test');
 
-function validation() {
-    var isError=false;
-    var divs= document.querySelectorAll('input.test');
-    [].forEach.call(divs, function(item) {
-    
-if(item.value.length == 0){
-       document.getElementById("error").innerHTML="La saisi des champs   est oblgatoire";
+[].forEach.call(divs, function(item) {
+
+ if(item.value.length == 0){
+       document.getElementById("error").innerHTML="La saisi des champs est oblgatoire";
        isError = true;
-    }
 
-        
-    else if(item.value.length<5){
-        document.getElementById("error").innerHTML="le nom doit contenir au moins 5 caracteres";
-        document.getElementById("error").removeAttribute("hidden");
-        isError=true;
-                             }
-                            
+   }
+   else if (item.value.length <5) {
+       document.getElementById("error").innerHTML="Les champs doivent contenir au moins 5 caracteres";
+       isError = true;
+   }
+   
+   
+   
+   
+});
+if(isError == false){
+       document.getElementById("ls").style.backgroundColor="green";
+document.getElementById("error").innerHTML="Bienvenue:"  + ""+ document.getElementById("nom").value;
+   }
 
-    });
-
-if(isError==false){
-    document.getElementById("resultat").style.backgroundColor ="green";
-document.getElementById("resultat").innerHTML="Bienvenue:"+ document.getElementById("nom").value;
-    
-    }
-         
-        
 }
-
-
-
-
-
-
-
